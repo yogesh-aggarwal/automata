db67c93fc6d7cba8038bb6a9fcc7760efca51351
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-08-29 21:05:35 +0530

    Added files

---
+++ src/models/auth.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+
+const tokenSchema = new mongoose.Schema({
+	access_token: String,
+	refresh_token: String,
+	scope: String,
+	token_type: String,
+	expiry_date: Number,
+})
+
+const schema = new mongoose.Schema({
+	_id: mongoose.Types.ObjectId,
+	email: String,
+})
+
+export const AuthModel = mongoose.model("Auth", schema, "auth")
